cda42a59eb7a3fa93ecbab93679efe228b918d02
Papa-Cristian <cristian.pap@golfgenius.com>
2021-12-29 15:22:44 +0200

    Fix linter errors

--- pages/golfers/[id].js
+++ pages/golfers/[id].js
@@ -8,21 +8,21 @@ const GolferScores = () => {
   const router = useRouter()
   const { id } = router.query
 
-  const { user, error_user } = useUser(id)
+  const { user, errorUser } = useUser(id)
   const { scores, error } = useScoresGolfer(id)
 
   return (
     <Layout>
       <>
         {
-          error_user ? (
-            error_user
+          errorUser ? (
+            errorUser
           ) : (
             <>
-            { 
-            user &&
-              <h1 className='text-3xl'>{`The scores of golfer ${user.name}`}</h1>
-            }
+              {
+                user &&
+                <h1 className="text-3xl">{`The scores of golfer ${user.name}`}</h1>
+              }
             </>
           )
         }
